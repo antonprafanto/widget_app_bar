4f0639710d537c63dad2ff9b48a7a00d44b04da7
antonprafanto <anton.prafanto@gmail.com>
2023-03-20 07:27:12 +0800

    Update

--- lib/page_satu.dart
+++ lib/page_satu.dart
@@ -17,11 +17,11 @@ class PageSatu extends StatelessWidget {
             style: TextStyle(color: Colors.black),
           ),
           //Container(height: 20,color: Colors.amber,),
-          //centerTitle: true,
-          //leading: Container(color: Colors.yellow,),leadingWidth: 100,
-          //actions: [Text("Tombol 1"),Text("Tombol 2"),],
-          //bottom: PreferredSize(preferredSize: Size.fromHeight(100),
-          //child: Container(height: 100,color: Colors.red,width: 20,),),
+          centerTitle: true,
+          leading: Container(color: Colors.yellow,),leadingWidth: 100,
+          actions: [Text("Tombol 1"),Text("Tombol 2"),],
+          bottom: PreferredSize(preferredSize: Size.fromHeight(100),
+          child: Container(height: 100,color: Colors.red,width: 20,),),
         ),
         body: Center(
           child: ElevatedButton(
